6ddb781a0c21de6067223e8ebe826902db736c27
Sarah Nadler <sarah.m.nadler@gmail.com>
2019-02-26 14:09:12 -0700

    tests for recipes added

--- test/test-server.js
+++ test/test-server.js
@@ -150,3 +150,116 @@ describe("Shopping List", function() {
     );
   });
 });
+
+describe("Recipes", function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
+  // test strategy:
+  //   1. make request to `/recipes`
+  //   2. inspect response object and prove has right code and have
+  //   right keys in response object.
+  it("should list recipes on GET", function() {
+    return chai
+      .request(app)
+      .get("/recipes")
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("array");
+
+
+        expect(res.body.length).to.be.at.least(1);
+
+        const expectedKeys = ["id", "name", "ingredients"];
+        res.body.forEach(function(item) {
+          expect(item).to.be.a("object");
+          expect(item).to.include.keys(expectedKeys);
+        });
+      });
+  });
+
+  // test strategy:
+  //  1. make a POST request with data for a new item
+  //  2. inspect response object and prove it has right
+  //  status code and that the returned object has an `id`
+  it("should add an recipe on POST", function() {
+    const newRecipe = { name: "burrito", ingredients: [
+      "beans",
+      "beef",
+      "peppers"
+    ] };
+    return chai
+      .request(app)
+      .post("/recipes")
+      .send(newRecipe)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a("object");
+        expect(res.body).to.include.keys("id", "name", "ingredients");
+        expect(res.body.id).to.not.equal(null);
+        expect(res.body).to.deep.equal(
+          Object.assign(newRecipe, { id: res.body.id })
+        );
+      });
+  });
+
+  // test strategy:
+  //  1. initialize some update data (we won't have an `id` yet)
+  //  2. make a GET request so we can get an item to update
+  //  3. add the `id` to `updateData`
+  //  4. Make a PUT request with `updateData`
+  //  5. Inspect the response object to ensure it
+  //  has right status code and that we get back an updated
+  //  item with the right data in it.
+  it("should update recipe on PUT", function() {
+    const updateData = {name: "foo", ingredients: [
+      "bizz",
+      "buzz",
+      "fizzbuzz"]
+    };
+
+    return (
+      chai
+        .request(app)
+        .get("/recipes")
+        .then(function(res) {
+          updateData.id = res.body[0].id;
+          return chai
+            .request(app)
+            .put(`/recipes/${updateData.id}`)
+            .send(updateData);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+          //expect(res).to.be.json; -- based on code it is not turned into json
+          expect(res.body).to.be.a("object");
+         // expect(res.body).to.deep.equal(updateData); -- based on code, recipe put doesnt return the object
+        })
+    );
+  });
+
+  // test strategy:
+  //  1. GET shopping list items so we can get ID of one
+  //  to delete.
+  //  2. DELETE an item and ensure we get back a status 204
+  it("should delete items on DELETE", function() {
+    return (
+      chai
+        .request(app)
+        .get("/recipes")
+        .then(function(res) {
+          return chai.request(app).delete(`/recipes/${res.body[0].id}`);
+        })
+        .then(function(res) {
+          expect(res).to.have.status(204);
+        })
+    );
+  });
+});
